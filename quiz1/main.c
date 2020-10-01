b849381726505fbc83158b9d1060cf3efeab59c3
baladaa10 <bks5681@psu.edu>
2020-10-01 02:29:57 +0000

    fixed it again!

--- quiz1/main.c
+++ quiz1/main.c
@@ -3,9 +3,12 @@
 #include <stdlib.h>
 #include <readline/readline.h>
 #include "quiz1.h"
-int main(void) {
-  int num = atoi(readline("Enter an integer: "));
-  printf("sum is %d.\n", sum_n(num));
-  char *line = readline("Enter a base: ");
-  printf("answer is %d.\n", digit_sum_base(n,base));
+
+int main(){
+  char * n = readline("Enter an integer: ");
+  char * base = readline("Enter a base: ");
+  int number = atoi(n);
+  int basenumber = atois(base);
+  printf("answer is %d.\n", digit_sum_base(number, basenumber));
 }
+
--- quiz1/quiz1.c
+++ quiz1/quiz1.c
@@ -1,7 +1,7 @@
 // Author: Balade Shala bks5681@psu.edu
 #include "quiz1.h"
 
-int digit_sum_base(int n, int base) {
+int digit_sum_base(int n, int base){
 int s = 0;
 while(n>0){
  s +=n%base;
