199b6c34a872682300c5759ba9c4e998cc174499
baladaa10 <bks5681@psu.edu>
2020-10-01 01:48:20 +0000

    I included what was needed

--- quiz1/main.c
+++ quiz1/main.c
@@ -1,7 +1,12 @@
-// Author: 
+// Author: Balade Shala bks5681@psu.edu
 #include <stdio.h>
 #include "quiz1.h"
 
-int main(void) {
-  return 0;
+int main(){
+int n, base;
+printf("Enter an int: ");
+scanf("%d",&n);
+printf("Enter a base: ");
+scanf("%d",&base);
+printf("Answer is %d.\n", digit_sum_base(n,base));
 }
--- quiz1/quiz1.c
+++ quiz1/quiz1.c
@@ -1,5 +1,18 @@
-// Author: 
+// Author: Balade Shala bks5681@psu.edu
 #include "quiz1.h"
+
 int digit_sum_base(int n, int base) {
-  return 0;
+int s = 0;
+while(n>0){
+  s +=n%base;
+}
+return s;
+}
+int main(){
+int n, base;
+printf("Enter an int: ");
+scanf("%d",&n);
+printf("Enter a base: ");
+scanf("%d",&base);
+printf("Answer is %d.\n", digit_sum_base(n,base));
 }
